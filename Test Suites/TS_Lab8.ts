93f1cbeef99cebc4b940a24317b3792adf0b12b8
KimChi <trinhkimchi456@gmail.com>
2020-10-07 13:57:30 +0700

    Add TestSuit Lab8

---
+++ Test Suites/TS_Lab8.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_Lab8</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>a5c43e20-aace-4356-9778-003311194eb6</testSuiteGuid>
+   <testCaseLink>
+      <guid>c1c07251-1793-46a1-8f17-6450f070b582</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Lab8</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>29747d88-af69-4ee6-958a-8667ce5360a4</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Add_data_katalon</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>29747d88-af69-4ee6-958a-8667ce5360a4</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Username</value>
+         <variableId>9332012e-74fb-4f60-a14a-98d72265e8f9</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>29747d88-af69-4ee6-958a-8667ce5360a4</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Password</value>
+         <variableId>b109ea93-95f3-4159-9978-0e7d614462bf</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
